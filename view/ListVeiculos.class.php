81de14fb87042d04915a73b92d9e523a535d2b78
ZataraWin <master@limabauru.com.br>
2017-09-20 13:19:43 -0300

    tabelas modificadas

--- view/ListVeiculos.class.php
+++ view/ListVeiculos.class.php
@@ -23,10 +23,12 @@ $veiculos2 = $controlVeiculo->listLimit($pagina2,2,2);
 <html>
 <head>
 	<title></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
 <body>
 	<div align="center">
-		<table border="1">
+		<table class="table table-sm table-responsive table-stripped">
 			<thead>
 				<th colspan="7">TÃO - <?php echo $numVeiculos1; ?> - VEÍCULOS</th>
 			</thead>
@@ -88,7 +90,7 @@ $veiculos2 = $controlVeiculo->listLimit($pagina2,2,2);
 	</div>
 	<hr>	
 	<div align="center">
-		<table border="1">
+		<table class=" table table-sm table-responsive">
 			<thead>
 				<th colspan="7">NÃO TÃO</th>
 			</thead>
@@ -123,6 +125,8 @@ $veiculos2 = $controlVeiculo->listLimit($pagina2,2,2);
 		}
 		?>
 	</div>
-
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
 </body>
 </html>
