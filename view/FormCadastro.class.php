4f679585b6eb0d5d0db9ff45b8d39b90552a01cc
ZataraWin <master@limabauru.com.br>
2017-09-22 15:17:04 -0300

    Cadastro Veiculo com bootstrap

--- view/FormCadastro.class.php
+++ view/FormCadastro.class.php
@@ -10,22 +10,47 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Novo Veiculo</title>
+	<link href="css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-	<form action="" method="post">
-		<label>Placa</label>
-		<input type="text" name="placa">
-		<br>
-		<label>Descricao</label>
-		<input type="text" name="descricao">
-		<br>
-		<label>Tipo</label>
-		<input type="text" name="tipo">
-		<br>
-		<input type="submit" name="btnGravar">
-		<br>
-	</form>
+	<div class="container-fluid">
+		<form action="" method="post">
+			<div class="row">
+				<div class="col-xs-3">
+					<div class="form-group">
+						<label for="placa">Placa</label>
+						<input type="text" class="form-control" id="placa" placeholder="Placa" name="placa">
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xs-3">
+					<div class="form-group">
+						<label for="descricao">Descricao</label>
+						<input type="text" class="form-control" id="descricao" placeholder="Descrição" name="descricao">
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xs-1">
+					<div class="form-group">
+						<label for="tipo">Tipo</label>
+						<input type="text" class="form-control" id="tipo" placeholder="Tipo" name="tipo">
+					</div>
+					<button type="submit" class="btn btn-default" name="btnGravar">Gravar</button>
+				</div>
+			</div>
+		</form>
+	</div>
+
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<!-- Include all compiled plugins (below), or include individual files as needed -->
+	<script src="js/bootstrap.min.js"></script>
 </body>
 </html>
 
